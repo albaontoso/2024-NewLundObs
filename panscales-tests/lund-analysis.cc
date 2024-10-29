6a63b516ec9edd638e9e5c2288e7e7a488aa7d69
aontoso <albasotoontoso@gmail.com>
2024-10-29 15:36:34 +0100

    actual exclusive lund plane analysis

--- panscales-tests/lund-analysis.cc
+++ panscales-tests/lund-analysis.cc
@@ -9,27 +9,22 @@ using namespace fjcore;
 using namespace fjcore::contrib;
 
 //----------------------------------------------------------------------
-/// Code to examine a couple of Soft Drop observables in e+e- events.
+/// Code to examine a couple of Lund observables in e+e- events.
 ///
 /// example of command line is 
 /**
-  build-double/example-ee-Lund -shower panglobal -beta 0 -process ee2qq \
-      -physical-coupling -rts 91.1876 -match-process -nev 100000 \
-      -out example-results/example-lund-ee.dat
+  ./lund-analysis -shower panglobal -beta 0 -process ee2qq \
+      -physical-coupling -rts 1000 -match-process -nev 100000 \
+      -out a
 */
 /// This should run in a few seconds.
-/// 
-/// To activate NNLL, one can run
-/** 
-  build-double/example-ee -shower panglobal -split-dipole-frame -beta 0 -process ee2qq \
-      -physical-coupling -nloops 3 -rts 91.1876 -nev 100000 \
-      -no-spin-corr -colour CFHalfCA -nnll-sudakov -match-process \
-      -out example-results/example-ee-nnll.dat
-*/
+
 class ExampleEELund : public  AnalysisFramework {
 public:
   JetDefinition _jet_def;
   RecursiveLundEEGenerator _lund_ee_gen; 
+  double ktmin,ktmax;
+  double logthmin, logthmax; // define the collinear window
   /// ctor
   ExampleEELund(CmdLine * cmdline_in)
     : AnalysisFramework(cmdline_in) {
@@ -44,40 +39,54 @@ public:
   /// options needed by the "user" analysis can be set here
   void user_startup() override {
     
-    // declare histograms and binning
-    Binning zg_binning(0.0, 0.5, 0.01);
-    cumul_hists_err["z" ].declare(zg_binning);   
-    Binning thg_binning(0.0, 1.0, 0.1); 
-    cumul_hists_err["theta"].declare(thg_binning);    
+    cmdline->start_section("Choice of input parameters"); //---------
 
+    // -- Lund plane windows that define the observable
+    ktmin     = cmdline->value("-kt-min", 2.);
+    ktmax     = cmdline->value("-kt-max", 200.);
+    logthmin  = cmdline->value("-lnth-min", 4);
+    logthmax  = cmdline->value("-lnth-max", 5);
+    // veto for the subleading jet based on pt and rapidity
+    cmdline->end_section("Choice of input parameters"); //-----------
+
+    // declare histograms 
+    hists_2d_compact["lnovth_lnkt_wo_coll" ].declare(0.0, 2*logthmax,1.0, log(ktmin), log(ktmax), 0.5);   
+    hists_2d_compact["lnovth_lnkt_w_coll"].declare(0.0, 2*logthmax, 1.0,  log(ktmin), log(ktmax), 0.5);    
   }
 
   //----------------------------------------------------------------------
   /// this gets called once for every event and should carry
   /// out the analysis and output histograms
   void user_analyse_event() override {
+    double evwgt = event_weight();
    // Main loop over the PanScales event
    std::vector<PseudoJet> particles; 
-   //std::cout << "Number of particles " << f_event.size() << std::endl;
     for (auto &p:f_event.particles()){
       PseudoJet q(p.px(),p.py(), p.pz(), p.E());
       if(abs(p.pdgid()) != 11) particles.push_back(q); // only final-state particles
     }
     // We cluster the event in C/A ycut=1 jets. 
-   // std::cout << "Number of particles " << particles.size() << std::endl;
     if(particles.size()<2) return;
     ClusterSequence cs(particles, _jet_def);
 
     // Obtain the declusterings 
     std::vector<LundEEDeclustering> declusterings;
     declusterings = _lund_ee_gen.result(cs);
-    // it should be only one in this case
-    //std::cout << "Number of declusterings " << declusterings.size() << std::endl;
+    // Record the Lund coordinates
+    bool coll_emission = false; // bool to check whether there are emissions in the collinear region 
+    std::vector<pair<double,double>> lund_coordinates;
     for (const auto & d : declusterings){
-        cumul_hists_err["z"].add_entry(d.z(), event_weight());
-        cumul_hists_err["theta"].add_entry(2*atan(exp(-d.eta())), event_weight());
+      pair<double,double> coords = d.lund_coordinates();
+      lund_coordinates.push_back(coords);
+      // Check whether there is an emission in the collinear region
+      if (coords.first > logthmin && coords.second < logthmax) coll_emission=true;
     }
-    
+    // NOTE: I guess there is a smart way of avoiding this second loop
+    for (const auto & l : lund_coordinates)
+      if (coll_emission){
+        hists_2d_compact["lnovth_lnkt_w_coll"].add_entry(l.first, l.second, evwgt); // There was at least one collinear emission
+      }
+      else hists_2d_compact["lnovth_lnkt_wo_coll"].add_entry(l.first, l.second, evwgt);
   }
 };
   
