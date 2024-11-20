6769b3873ef8162368e4b22c0c3fa4011da07659
Alba Soto Ontoso <asotoont@lxplus951.cern.ch>
2024-11-20 18:07:49 +0100

    a few changes to allow for multiple seeds

--- panscales-tests/run-lund.sh
+++ panscales-tests/run-lund.sh
@@ -1,8 +1,10 @@
 nevents=(1)
-
+nruns=(10)
 for nevent in "${nevents[@]}" 
 do
-    sed -e "s/&nev&/${nevent}/g" run-lund-lxplus.sub > run-lund-lxplus.tmp
+    nrun="${nruns[iruns]}"
+    sed -e "s/&nev&/${nevent}/g" -e "s/&njobs&/$nrun/g" run-lund-lxplus.sub > run-lund-lxplus.tmp
     condor_submit run-lund-lxplus.tmp
     rm run-lund-lxplus.tmp
+    iruns=$iruns+1
 done
