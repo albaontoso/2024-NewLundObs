d04210f8a8476b9ec29b9ea7c12897cd6d951b18
Alba Soto Ontoso <asotoont@lxplus904.cern.ch>
2024-11-21 18:52:47 +0100

    fixed typos

--- panscales-tests/run-lund.sh
+++ panscales-tests/run-lund.sh
@@ -2,8 +2,8 @@ etamins=(5 6 7 8)
 njob=10
 for etamin in "${etamins[@]}" 
 do
-    etamax=$etamin+1
-    sed -e "s/&ymin&/${etamin}/g" -e "s/&ymax&/$etamax/g" -e "s/&njobs&/$njob/g"  run-lund-lxplus.sub > run-lund-lxplus.tmp
+    etamax=$(($etamin+1))
+    sed -e "s/&ymin&/${etamin}/g" -e "s/&ymax&/${etamax}/g" -e "s/&njobs&/$njob/g"  run-lund-lxplus.sub > run-lund-lxplus.tmp
     condor_submit run-lund-lxplus.tmp
     rm run-lund-lxplus.tmp
 done
